11ad7933e53dd42529142818763ff2e14d3dd9ff
Andrecodev <andrescriollo.ac7@gmail.com>
2025-08-27 20:58:26 -0500

    feat(Firebase): Set up firebase envs into .env,

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app"
 import { getAuth, GoogleAuthProvider } from "firebase/auth"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBl6SYjXQrkBDpvmv04vaS2Zdx82KCZhM0",
-  authDomain: "learnai-5da33.firebaseapp.com",
-  projectId: "learnai-5da33",
-  storageBucket: "learnai-5da33.firebasestorage.app",
-  messagingSenderId: "936613703151",
-  appId: "1:936613703151:web:42569190009483a4795a1a",
-  measurementId: "G-109FNSFV4Z",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 }
 
 // Initialize Firebase
